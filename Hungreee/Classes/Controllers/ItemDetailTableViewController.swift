0c98578d31c8270e31db64487b0e752ba926d64d
Hiroki Matsue <hiroki.matsue@gmail.com>
2015-06-27 23:35:17 +0900

    Add restaurant icon on detail page and add a new logo

--- Hungreee/Classes/Controllers/ItemDetailTableViewController.swift
+++ Hungreee/Classes/Controllers/ItemDetailTableViewController.swift
@@ -20,12 +20,7 @@ class ItemDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
     }
     
     override func viewWillAppear(animated: Bool) {
--- Hungreee/Classes/Views/ItemDetail/ItemDetailImageTableViewCell.swift
+++ Hungreee/Classes/Views/ItemDetail/ItemDetailImageTableViewCell.swift
@@ -11,10 +11,15 @@ import UIKit
 class ItemDetailImageTableViewCell: UITableViewCell {
 
     @IBOutlet weak private(set) var itemImageView: UIImageView!
+    @IBOutlet weak private(set) var shopImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        shopImageView.layer.borderWidth = 3.0
+        shopImageView.layer.borderColor = UIColor.whiteColor().CGColor
+        shopImageView.layer.cornerRadius = shopImageView.frame.size.width / 2
+        shopImageView.clipsToBounds = true
+        self.bringSubviewToFront(shopImageView)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -27,6 +32,7 @@ class ItemDetailImageTableViewCell: UITableViewCell {
     
     func constructWithItem(item: Item) {
         itemImageView.sd_setImageWithURL(NSURL(string: item.imageUrl))
+        shopImageView.sd_setImageWithURL(NSURL(string: item.imageUrl))
     }
     
 }
