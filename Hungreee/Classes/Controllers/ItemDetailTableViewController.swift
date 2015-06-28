387332364af4207b36fba3d3da2b9ff5ddfd08a6
Hiroki Matsue <hiroki.matsue@gmail.com>
2015-06-28 10:54:16 +0900

    Update some icons

--- Hungreee/Classes/Controllers/ItemDetailTableViewController.swift
+++ Hungreee/Classes/Controllers/ItemDetailTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SDWebImage
 
-class ItemDetailTableViewController: UITableViewController {
+class ItemDetailTableViewController: UITableViewController, ItemDetailPaymentsTableViewCellDelegate {
     
     private var item: Item!
     private var itemDescriptionTableViewCell: ItemDetailDescriptionTableViewCell!
@@ -93,6 +93,15 @@ class ItemDetailTableViewController: UITableViewController {
         return height
     }
     
+    // MARK: ItemDetailPaymentsTableViewCellDelegate
+    
+    func itemDetailPaymentsTableViewCellDelegate(itemDetailPaymentsTableViewCell: ItemDetailPaymentsTableViewCell, didClickApplyButton: UIButton) {
+        let waitingViewController = storyboard?.instantiateViewControllerWithIdentifier("WaitingViewControllerID") as! WaitingViewController
+        navigationController?.pushViewController(waitingViewController, animated: true)
+    }
+    
+    // MARK: Private
+    
     private func createItemDescriptionTableViewCellIfNeeded(item: Item) -> ItemDetailDescriptionTableViewCell {
         if itemDescriptionTableViewCell == nil {
             itemDescriptionTableViewCell = tableView.dequeueReusableCellWithIdentifier("ItemDetailDescriptionTableViewCellID") as? ItemDetailDescriptionTableViewCell
@@ -111,49 +120,4 @@ class ItemDetailTableViewController: UITableViewController {
         return itemDetailPaymentsTableViewCell;
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
